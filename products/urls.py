41e43732d2efbc86d0bbd6953fe93392fd18678f
ChauhanSiddharth <siddharth.chauhan50@yahoo.com>
2019-04-05 20:15:06 -0400

    project min req completion

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static, settings
+from .views import create, detail, upvote
+
+urlpatterns = [
+    path('create/', create, name='create'),
+    path('<int:product_id>', detail, name='detail'),
+    path('<int:product_id>/upvote', upvote, name='upvote'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
